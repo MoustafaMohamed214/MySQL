efd3342abad7e4d882eb2e394553bc173defc121
MoustafaMohamed214 <moustafa1999ahmed@gmail.com>
2024-11-14 15:42:04 +0100

    Delete IN.sql

--- IN.sql
+++
@@ -1,7 +0,0 @@
-SELECT name, primary_poc, sales_rep_id
-FROM accounts
-WHERE name IN ('Walmart', 'Target', 'Nordstrom');
------------------------------------------------------------------------------------------------------------
-SELECT *
-FROM web_events
-WHERE channel IN ('organic', 'adwords');
