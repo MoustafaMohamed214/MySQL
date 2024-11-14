c8b591d43f09d1d45e971489f01b85140bd041b3
MoustafaMohamed214 <moustafa1999ahmed@gmail.com>
2024-11-14 15:38:17 +0100

    Add files via upload

---
+++ Basic SQL/WHERE.sql
@@ -0,0 +1,13 @@
+SELECT *
+FROM orders
+WHERE gloss_amt_usd >= 1000
+LIMIT 5;
+-------------------------------------------------------------------------------------------------
+SELECT *
+FROM orders
+WHERE total_amt_usd < 500
+LIMIT 10;
+-------------------------------------------------------------------------------------------------
+SELECT name, website, primary_poc
+FROM accounts
+WHERE name = 'Exxon Mobil'
