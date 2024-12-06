ca9cbaff26ee843e51affe37ccfd55f8f716b9a9
MoustafaMohamed214 <moustafa1999ahmed@gmail.com>
2024-12-06 21:25:17 +0100

    Update Codes.sql

--- Sub_Queries/Codes.sql
+++ Sub_Queries/Codes.sql
@@ -1,1 +1,47 @@
+'1. Use DATE_TRUNC to pull month level info about the first order ever placed in the orders table'
+
+  SELECT DATE_TRUNC('month', MIN(occurred_at)) 
+FROM orders;
+
+'2. Use the prev query to find only the orders that took place in the same month and year as 
+  the first order,and then pull the average for each type of paper (qty) in this month'
+
+  SELECT AVG(standard_qty) avg_std, AVG(gloss_qty) avg_gls, AVG(poster_qty) avg_pst
+FROM orders
+WHERE DATE_TRUNC('month', occurred_at) = 
+     (SELECT DATE_TRUNC('month', MIN(occurred_at)) FROM orders);
+
+'For calculating sum'
+SELECT SUM(total_amt_usd)
+FROM orders
+WHERE DATE_TRUNC('month', occurred_at) = 
+      (SELECT DATE_TRUNC('month', MIN(occurred_at)) FROM orders);
+  
+
+
+'1. Provide the name of the sales_rep in each region with the largest amount of total_amt_usd sales.'
+   
+SELECT t3.rep_name, t3.region_name, t3.total_amt
+FROM(SELECT region_name, MAX(total_amt) total_amt
+        FROM(SELECT s.name rep_name, r.name region_name, SUM(o.total_amt_usd) total_amt
+                FROM sales_reps s
+                JOIN accounts a
+                ON a.sales_rep_id = s.id
+                JOIN orders o
+                ON o.account_id = a.id
+                JOIN region r
+                ON r.id = s.region_id
+                GROUP BY 1, 2) t1
+        GROUP BY 1) t2
+JOIN (SELECT s.name rep_name, r.name region_name, SUM(o.total_amt_usd) total_amt
+        FROM sales_reps s
+        JOIN accounts a
+        ON a.sales_rep_id = s.id
+        JOIN orders o
+        ON o.account_id = a.id
+        JOIN region r
+        ON r.id = s.region_id
+        GROUP BY 1,2
+        ORDER BY 3 DESC) t3
+ON t3.region_name = t2.region_name AND t3.total_amt = t2.total_amt;
 
